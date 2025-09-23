190e3cc0dedc85bcd39bd0a3eb6f02e49da8584a
huwenjie912 <huwenjie912@gmail.com>
2025-09-23 18:11:39 +0800

    add treasury and coin

---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes, AccessControl, Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(address admin)
+        ERC20("CharityDAO Governance", "GOV")
+        ERC20Permit("CharityDAO Governance")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    }
+
+    function pause() external onlyRole(DEFAULT_ADMIN_ROLE) { _pause(); }
+    function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) { _unpause(); }
+
+    function mintOnDonation(address to, uint256 amount, bytes32 donationId)
+        external
+        onlyRole(MINTER_ROLE)
+    {
+        require(to != address(0) && amount > 0, "bad params");
+        _mint(to, amount);
+        emit MintedOnDonation(to, amount, donationId);
+    }
+
+    // --- OZ hooks
+    function _update(address from, address to, uint256 value)
+        internal override(ERC20, ERC20Votes)
+        whenNotPaused
+    { super._update(from, to, value); }
+
+    event MintedOnDonation(address indexed to, uint256 amount, bytes32 donationId);
+}
